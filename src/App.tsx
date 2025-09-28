4bda4ec42b0dd1bab5d0d82c9113209c6838ed48
Darshan Thakare <143271270+DarshanCode2005@users.noreply.github.com>
2025-09-29 02:54:04 +0530

    BrowserRouter changed

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/18SEE">
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/schedule" element={<Schedule />} />
